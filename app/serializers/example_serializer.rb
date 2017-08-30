0f73479cbc80ced10c7903da272ceb0b1d3b93da
MicFin <mrfinneran@gmail.com>
2017-08-29 23:19:31 -0400

    Discourage developers from serializing out password digest
    Close #54

--- app/serializers/example_serializer.rb
+++ app/serializers/example_serializer.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
-class ExampleSerializer < ActiveModel::Serializer
-  attributes :id, :text, :editable
 
+class ExampleSerializer < ActiveModel::Serializer
+  attributes :id, :text, :editable # :user
+  #           WORST (do not do)------^^^^
+
+  # belongs_to :user
+  #             ^^^^----ACCEPTABLE (when BEST isn't an option)
+
+  # BEST
   def editable
     scope == object.user
   end
