2040f398a0f42f950d3d94644e0cef449fae72f8
MicFin <mrfinneran@gmail.com>
2018-10-02 14:45:06 -0400

    Remove users curl scripts
    End points previously removed

--- curl-scripts/users/user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost:4741/users/${ID}" \
-  --include \
-  --request GET \
-  --header "Authorization: Token token=${TOKEN}"
-
-echo
--- curl-scripts/users/users.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost:4741/users" \
-  --include \
-  --request GET \
-  --header "Authorization: Token token=${TOKEN}"
-
-echo
